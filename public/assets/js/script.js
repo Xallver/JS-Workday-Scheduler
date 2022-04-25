dc0efad6efa9d69ecc66edef45c29d6abab17f6d
Xallver <jamesstanley11@gmail.com>
2022-04-24 22:53:48 -0400

    found bug in my save function which wasn't properly saving my notes

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,9 +1,9 @@
-$(document).ready(function () {
+$(document).ready(function() {
     // save button click listner
-    $('.saveBtn').on('click', function () {
+    $('.saveBtn').on('click', function(){
 
         //this gets textarea values from desc. class
-        var value = $(this).siblings('description').val()
+        var value = $(this).siblings('.description').val()
 
         //this gets id value from time-block which gets converted into a number
         var time = $(this).parent().attr('id');
@@ -28,7 +28,7 @@ $(document).ready(function () {
             //else if (block === currentHour) remove .past class & add .present class to the text area of time-block
             else if (blockHour === currentHour) {
                 $(this).children('textarea').removeClass('past');
-                $(this).children('textarea').addClass('present');
+                $(this).children('textarea').addClass('present')
             }
             //else (blockHour > currentHour) remove .past class & add .present/ .future class to the text area of .time-block
             else {
@@ -48,7 +48,7 @@ $(document).ready(function () {
     $('.time-block').each( function(){
         var id = $(this).attr('id')
         $(this).children('textarea').val(localStorage.getItem(id))
-    });
+    })
 
     // this displays current day in header of webpage
     $("#currentday").text(moment().format("dddd, MMMM Do"));
